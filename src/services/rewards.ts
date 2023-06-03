c41bb33839446cb7520b2445150ee58f4e1b9dbc
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2023-06-03 22:03:08 +0300

    update rewards API route with getUserRewardsQuery

--- src/services/rewards.ts
+++ src/services/rewards.ts
@@ -1,8 +1,11 @@
-import { localService } from "@/services/utils";
-import { IRewards } from "@/types";
+import { localService } from "@/services/api";
+import { IReward } from "@/types";
 
 export const getRewardsQuery = async () =>
   localService.get(`/rewards`).then((res) => res.data);
 
-export const addRewardQuery = async (data: IRewards) =>
+export const getUserRewardsQuery = async (email: string) =>
+  localService.get(`/rewards?email=${email}`).then((res) => res.data);
+
+export const addRewardQuery = async (data: IReward) =>
   localService.post("/rewards", data);
