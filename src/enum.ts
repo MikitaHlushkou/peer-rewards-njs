6f56ec50afa44296488df87b1051645c4d5585a0
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2023-06-03 21:29:45 +0300

    add AUTH and QUERY_KEYS enums

--- src/enum.ts
+++ src/enum.ts
@@ -1,8 +1,17 @@
 export enum INPUT_TYPES {
-  "textArea" = "TextArea",
-  "input" = "Input",
+  textArea = "TextArea",
+  input = "Input",
+  select = "Select",
 }
 
 export enum QUERY_KEYS {
-  "rewards",
+  rewards,
+  userRewards,
+  usersList,
+  userProfile,
+}
+
+export enum AUTH_CREDS_PROVIDERS {
+  google = "google",
+  credentials = "credentials",
 }
